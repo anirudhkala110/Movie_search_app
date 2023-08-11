b5dc78e50dfc5b7488faf18576be4c6a5ed357f8
anirudhkala110 <anirudhkala110@gmail.com>
2023-08-11 14:09:28 +0530

    Some Changes made in Detailing Page

--- frontend/src/components/Details/Detail.jsx
+++ frontend/src/components/Details/Detail.jsx
@@ -6,15 +6,16 @@ function Detail({ selected, close }) {
 
             <div className="d-flex flex-column justify-content-center text-light">
                 <div className="container bg-dark p-3 mt-5 p-5">
-                    <div className="row border-light p-5 " style={{boxShadow:"#00000069 0px 3px 3px"}}>
+                    <div className="row border-light p-5 " style={{ boxShadow: "#00000069 0px 3px 3px" }}>
                         <div className="col-10 col-md-5 m-3 border-light text-center d-flex justify-content-center align-items-center">
-                            <img src={selected.Poster} className='imghover'/>
+                            <img src={selected.Poster} className='imghover' />
                         </div>
                         <div className="col-12 col-md-6 m-3">
                             <h2>{selected.Title}</h2>
-                            <p><strong>Year :</strong> {selected.Year}</p>
-                            <p><strong>Rating :</strong> {selected.imdbRating}</p>
-                            <p><strong>Description :</strong> {selected.Plot}</p>
+                            <p><strong>Year &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</strong> {selected.Year}</p>
+                            <p><strong>Type &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</strong> {selected.Type}</p>
+                            <p><strong>Rating &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</strong> {selected.imdbRating}</p>
+                            <p><strong>Description &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</strong> {selected.Plot}</p>
                             <button onClick={close} className='btn btn-danger mt-5'>Close</button>
                         </div>
                     </div>
--- frontend/src/components/Result/Result.jsx
+++ frontend/src/components/Result/Result.jsx
@@ -4,8 +4,8 @@ function Result({ result, openDetail }) {
     return (
         <>
 
-            <div className="result m-3" onClick={e => openDetail(result.imdbID)}>
-                <div className=" border border-dark border-3 px-5 bg-dark pt-3">
+            <div className="result m-3 border" style={{background:"black"}} onClick={e => openDetail(result.imdbID)}>
+                <div className="pt-3 shadow image">
                     <img src={result.Poster} alt="" />
                 </div>
                 <div className=" bg-dark text-light p-3 ">
